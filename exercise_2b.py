249d525969a322e8c57bb7760f5584f9931ee75a
yma00520 <150973509+yma00520@users.noreply.github.com>
2024-01-17 16:04:48 -0700

    Update exercise_2b.py

--- exercise_2b.py
+++ exercise_2b.py
@@ -1,55 +1,44 @@
-def earthquake_data(filename):
-    data = []
-    with open(filename, 'r') as file:
+import os
+
+
+def read_earthquakes(file_name):
+    script_path = os.path.abspath(__file__)
+    file_path = os.path.join(os.path.dirname(script_path), file_name)
+    earthquakes = {}
+    with open(file_path, "r") as file:
         for line in file:
-            magnitude, date, time, latitude, longitude, depth, *region_parts = line.split(' ') 
-            region = ''.join(region_parts[-1])
-            data.append((region.strip(), [date, float(magnitude)]))
-    return data 
+            (
+                magnitude,
+                date,
+                _,
+                _,
+                _,
+                _,
+                *region_parts,
+            ) = line.split()
+            region = region_parts[-1].strip()
+            region_earthquakes = earthquakes.get(region, [])
+            region_earthquakes.append({"date": date, "magnitude": magnitude})
+            earthquakes[region] = region_earthquakes
+    return earthquakes
 
-def alaska_data(data):
-    alaska = []
-    alaska.append(data[0][0])
-    alaska.append(data[0][1])
-    alaska.append(data[3][1])
-    alaska.append(data[5][1])
-    alaska.append(data[7][1])
-    alaska.append(data[8][1])
-    return alaska
 
-def hawaii_data(data):
-    hawaii = []
-    hawaii.append(data[1][0])
-    hawaii.append(data[1][1])
-    hawaii.append(data[6][1])
-    return hawaii
+def write_earthquakes(earthquakes, file_name):
+    script_path = os.path.abspath(__file__)
+    file_path = os.path.join(os.path.dirname(script_path), file_name)
+    # Open a file in 'w' (write) mode
+    with open(file_path, "w") as file:
+        # Write lines to the file
+        for region, region_earthquakes in earthquakes.items():
+            file.write(f"[{region}")
+            for earthquake in region_earthquakes:
+                file.write(f", [{earthquake['date']}, {earthquake['magnitude']}]")
+            file.write(']\n')
 
-def mexico_data(data):
-    mexico = []
-    mexico.append(data[-2][0])
-    mexico.append(data[-2][1])
-    mexico.append(data[-1][1])
-    return mexico
-
-def region_data(data, region_indexs):
-    region_data = []
-    for index in region_indexs:
-        region_data.append(data[index][0])
-        region_data.append(data[index][1])
-    return region_data
-        
 def main():
-    panama_index = [2]
-    missouri_index = [4]
-    indonesia_index = [9]
-    vanuatu_index = [10]
-    data = earthquake_data('earthquake.txt') 
-    alaska = alaska_data(data)
-    panama = region_data(data, panama_index)
-    missouri = region_data(data, missouri_index)
-    indonesia = region_data(data, indonesia_index)
-    vanuatu = region_data(data, vanuatu_index)
-    hawaii = hawaii_data(data)
-    mexico = mexico_data(data)
-    
-main()
+    earthquakes = read_earthquakes("earthquake.txt")
+    write_earthquakes(earthquakes, "earthquakefmt.txt")
+
+
+if __name__ == "__main__":
+    main()
